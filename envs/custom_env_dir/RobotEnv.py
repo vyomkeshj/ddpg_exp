f592b07660c915dec5eb7cb01f90a085b4683ee1
rob-ot <vyomkeshjha@yahoo.com>
2020-05-11 08:19:40 +0200

    working TD3

--- envs/custom_env_dir/RobotEnv.py
+++ envs/custom_env_dir/RobotEnv.py
@@ -13,9 +13,9 @@ class RobotEnv(gym.Env):
         self.socket.connect("tcp://localhost:5555")
         self._max_episode_steps = 100
         print('Environment initialized')
-        self.action_space = spaces.Box(low=np.array([-1.0000, -1.0000]), high=np.array([1.0000, 1.0000]), dtype=np.float32)
-        self.observation_space = spaces.Box(low=np.array([-math.pi, -math.pi]),
-                                            high=np.array([math.pi, math.pi]),
+        self.action_space = spaces.Box(low=np.array([-4.0000, -4.0000]), high=np.array([4.0000, 4.0000]), dtype=np.float32)
+        self.observation_space = spaces.Box(low=np.array([-math.pi, -math.pi, -1, -1, -1]),
+                                            high=np.array([math.pi, math.pi, 1, 1, 1]),
                                             dtype=np.float32)
 
     def step(self, action):
@@ -23,7 +23,7 @@ class RobotEnv(gym.Env):
         message = self.socket.recv().decode("utf-8")
         data = message.split(',');
         float_data = list(map(lambda x: float(x), data))
-        obs = np.array(float_data[0:2])
+        obs = np.array(float_data[0:5])
 
         obs[0] = (3.14/180)*obs[0];
         obs[1] = (3.14/180)*obs[1];
@@ -31,10 +31,9 @@ class RobotEnv(gym.Env):
         #print("count=", self.action_count)
         #print("action command= %.10f, %.10f" % (action[0], action[1]))
         #print("observation received=",obs/3.14)
-        #print("reward received=",float_data[2]*0.1)
-        #print("done status=",float_data[3])
+        #print("done status=",float_data)
 
-        return obs/3.14, float_data[2]*0.1, float_data[3], {}
+        return obs, float_data[5]*0.100, float_data[6], {}
 
     def reset(self):
         #print('Environment reset')
@@ -43,7 +42,7 @@ class RobotEnv(gym.Env):
         #("Received respose: %s" % message)
         data = message.split(',');
         float_data = list(map(lambda x: float(x), data))
-        obs = np.array(float_data[0:2])
+        obs = np.array(float_data[0:5])
         return obs
 
     def render(self, mode='human'):
--- notebooks/TD3_2/main.py
+++ notebooks/TD3_2/main.py
@@ -8,7 +8,6 @@ import os
 # A fixed seed is used for the eval environment
 from notebooks.TD3_2 import TD3, utils
 
-
 def eval_policy(policy, env_name, seed, eval_episodes=10):
 	eval_env = env_name;
 	eval_env.seed(seed + 100)
@@ -36,7 +35,7 @@ if __name__ == "__main__":
 	parser.add_argument("--seed", default=0, type=int)              # Sets Gym, PyTorch and Numpy seeds
 	parser.add_argument("--start_timesteps", default=25e3, type=int)# Time steps initial random policy is used
 	parser.add_argument("--eval_freq", default=5e3, type=int)       # How often (time steps) we evaluate
-	parser.add_argument("--max_timesteps", default=1e5, type=int)   # Max time steps to run environment
+	parser.add_argument("--max_timesteps", default=3e5, type=int)   # Max time steps to run environment
 	parser.add_argument("--expl_noise", default=0.1)                # Std of Gaussian exploration noise
 	parser.add_argument("--batch_size", default=256, type=int)      # Batch size for both actor and critic
 	parser.add_argument("--discount", default=0.99)                 # Discount factor
@@ -47,6 +46,8 @@ if __name__ == "__main__":
 	parser.add_argument("--save_model", action="store_true")        # Save model and optimizer parameters
 	parser.add_argument("--load_model", default="")                 # Model load file name, "" doesn't load, "default" uses file_name
 	args = parser.parse_args()
+
+
 
 	file_name = "{args.policy}_{args.env}_{args.seed}"
 	print("---------------------------------------")
@@ -116,9 +117,9 @@ if __name__ == "__main__":
 		# Perform action
 		next_state, reward, done, _ = env.step(action)
 
-		print(done)
 		done_bool = float(done) if episode_timesteps < env._max_episode_steps else 0
 
+		epoch_end = False if episode_timesteps < env._max_episode_steps else True
 		# Store data in replay buffer
 		replay_buffer.add(state, action, next_state, reward, done_bool)
 
@@ -129,7 +130,7 @@ if __name__ == "__main__":
 		if t >= args.start_timesteps:
 			policy.train(replay_buffer, args.batch_size)
 
-		if done:
+		if bool(done) | epoch_end:
 			# +1 to account for 0 indexing. +0 on ep_timesteps since it will increment +1 even if done=True
 			print("Total T:", {t+1}," Episode Num:", {episode_num+1}," Episode T:", {episode_timesteps}," Reward:", {episode_reward})
 			# Reset environment
@@ -137,4 +138,4 @@ if __name__ == "__main__":
 			episode_reward = 0
 			episode_timesteps = 0
 			episode_num += 1 
-
+			policy.save("./models/model-"+str(episode_num))
