3e83787343363690b9dd7ec62e6a038050d59790
rob-ot <vyomkeshjha@yahoo.com>
2020-05-11 09:02:54 +0200

    working TD3

--- notebooks/TD3_2/main.py
+++ notebooks/TD3_2/main.py
@@ -90,52 +90,13 @@ if __name__ == "__main__":
 		kwargs["noise_clip"] = args.noise_clip * max_action
 		kwargs["policy_freq"] = args.policy_freq
 		policy = TD3.TD3(**kwargs)
-
+		policy.load("./models/model-"+str(7812))
 	replay_buffer = utils.ReplayBuffer(state_dim, action_dim)
 
 	# Evaluate untrained policy
-	#evaluations = [eval_policy(policy, env, args.seed)]
+	evaluations = [eval_policy(policy, env, args.seed)]
 
 	state, done = env.reset(), False
 	episode_reward = 0
 	episode_timesteps = 0
 	episode_num = 0
-
-	for t in range(int(args.max_timesteps)):
-
-		episode_timesteps += 1
-
-		# Select action randomly or according to policy
-		if t < args.start_timesteps:
-			action = env.action_space.sample()
-		else:
-			action = (
-				policy.select_action(np.array(state))
-				+ np.random.normal(0, max_action * args.expl_noise, size=action_dim)
-			).clip(-max_action, max_action)
-
-		# Perform action
-		next_state, reward, done, _ = env.step(action)
-
-		done_bool = float(done) if episode_timesteps < env._max_episode_steps else 0
-
-		epoch_end = False if episode_timesteps < env._max_episode_steps else True
-		# Store data in replay buffer
-		replay_buffer.add(state, action, next_state, reward, done_bool)
-
-		state = next_state
-		episode_reward += reward
-
-		# Train agent after collecting sufficient data
-		if t >= args.start_timesteps:
-			policy.train(replay_buffer, args.batch_size)
-
-		if bool(done) | epoch_end:
-			# +1 to account for 0 indexing. +0 on ep_timesteps since it will increment +1 even if done=True
-			print("Total T:", {t+1}," Episode Num:", {episode_num+1}," Episode T:", {episode_timesteps}," Reward:", {episode_reward})
-			# Reset environment
-			state, done = env.reset(), False
-			episode_reward = 0
-			episode_timesteps = 0
-			episode_num += 1 
-			policy.save("./models/model-"+str(episode_num))
